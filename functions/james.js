d418f87139c8a2a7b5652af9bca6fb3b8edcd509
James Sherry <james.m.sherry@googlemail.com>
2022-08-08 21:14:49 +0100

    james fn3

--- functions/james.js
+++ functions/james.js
@@ -3,7 +3,6 @@ exports.handler = (event, context, callback) => {
   console.log("Hello server logs");
   return callback(null, {
     statusCode: 200,
-    headers,
     body: JSON.stringify({ name: "James" }),
   });
 
